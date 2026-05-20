28d2a6978263c8e0d9367f3676884dfe7ffe3077
VinnieT04 <you@example.com>
2026-05-20 14:34:17 +0800

    working :)

--- visualizer/sketch.js
+++ visualizer/sketch.js
@@ -1,4 +1,4 @@
-let mood = "tense"
+let mood = null
 let energy = null
 
 function setMood(m)
@@ -17,6 +17,8 @@ function setup()
 
 function drawBars(inner, barLength)
 {
+    push()
+    
     translate(windowWidth / 2, windowHeight / 2)
     let outer = inner + (energy * barLength)
 
@@ -32,6 +34,8 @@ function drawBars(inner, barLength)
 
         line(x1, y1, x2, y2)
     }
+
+    pop()
 }
 
 function draw() 
@@ -67,37 +71,30 @@ function draw()
     }
 }
 
-const input = document.querySelector('#audio-file')
-input.addEventListener('change', (e) => {
-    const file = e.target.files[0]
-    const formData = new FormData()
-    formData.append("audio", file)
-
-    //fetch
-    fetch("http://localhost:8000/classify",{
-        method: "POST",
-        body: formData
-    })
-
-    .then(res => res.json())
-    .then(data => {
-        setMood(data.mood)
-        setEnergy(data.features.energy)
-    })
-})
-
 const audioEl = new Audio()
 
 document.querySelector('#play-btn').addEventListener('click', () => {
     audioEl.paused ? audioEl.play() : audioEl.pause()
 })
 
+document.querySelector('#volume').addEventListener('input', (e) => {
+    audioEl.volume = e.target.value
+})
+
 document.querySelector('#audio-file').addEventListener('change', (e) => {
     const file = e.target.files[0]
     audioEl.src = URL.createObjectURL(file)
-    // ... rest of your fetch code
-})
 
-document.querySelector('#volume').addEventListener('input', (e) => {
-    audioEl.volume = e.target.value
+    const formData = new FormData()
+    formData.append("file", file)
+
+    fetch("http://localhost:8000/classify", {
+        method: "POST",
+        body: formData
+    })
+    .then(res => res.json())
+    .then(data => {
+        setMood(data.mood)
+        setEnergy(data.features.energy)
+    })
 })
