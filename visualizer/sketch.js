bf714a46352eca0b3ad67937c2f65241bf7d32bb
VinnieT04 <you@example.com>
2026-05-20 13:37:44 +0800

    change css

--- visualizer/sketch.js
+++ visualizer/sketch.js
@@ -1,35 +1,65 @@
-let mood = null
+let mood = "tense"
+let energy = null
 
-function setMood(m){
+function setMood(m)
+{
     mood = m
+}
+function setEnergy(e)
+{
+    energy = e
 }
 
 function setup() 
 {    
-    createCanvas(400, 400);
+    createCanvas(windowWidth, windowHeight);
+}
+
+function drawBars(inner, barLength)
+{
+    translate(windowWidth / 2, windowHeight / 2)
+    let outer = inner + (energy * barLength)
+
+    for (let i = 0; i < 50; i++) 
+    {   
+        let angle = (i/50) * TWO_PI
+        //start of bar
+        let x1 = cos(angle) * inner
+        let y1 = sin(angle) * inner
+        // end of bar
+        let x2 = cos(angle) * outer
+        let y2 = sin(angle) * outer
+
+        line(x1, y1, x2, y2)
+    }
 }
 
 function draw() 
 {
     switch(mood){
         case "euphoric":
-            background('purple');
+            background('purple')
+            drawBars(80, 230)
             break;
 
         case "calm":
-            background('green');
+            background('green')
+            drawBars(80,180)
             break;
 
         case "aggressive":
-            background('red');
+            background('red')
+            drawBars(80, 300)
             break;
 
         case "melancholic":
-            background('blue');
+            background('blue')
+            drawBars(80, 150)
             break;
 
         case "tense":
             background('orange')
+            drawBars(80, 260)
             break;
 
         default:
@@ -44,14 +74,30 @@ input.addEventListener('change', (e) => {
     formData.append("audio", file)
 
     //fetch
-    fetch("http://localhost:8000/MULTIMEDIA_FINAL_PROJECT",{
+    fetch("http://localhost:8000/classify",{
         method: "POST",
         body: formData
     })
 
     .then(res => res.json())
     .then(data => {
-        console.log(data.mood)
         setMood(data.mood)
+        setEnergy(data.features.energy)
     })
+})
+
+const audioEl = new Audio()
+
+document.querySelector('#play-btn').addEventListener('click', () => {
+    audioEl.paused ? audioEl.play() : audioEl.pause()
+})
+
+document.querySelector('#audio-file').addEventListener('change', (e) => {
+    const file = e.target.files[0]
+    audioEl.src = URL.createObjectURL(file)
+    // ... rest of your fetch code
+})
+
+document.querySelector('#volume').addEventListener('input', (e) => {
+    audioEl.volume = e.target.value
 })
