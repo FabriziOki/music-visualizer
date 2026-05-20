5e19733e94843059da811c467648141280d44e7e
VinnieT04 <you@example.com>
2026-05-20 17:06:23 +0800

    almost done with the visualizer

--- visualizer/sketch.js
+++ visualizer/sketch.js
@@ -1,5 +1,6 @@
 let mood = null
 let energy = null
+let rotation = 0
 
 function setMood(m)
 {
@@ -15,15 +16,19 @@ function setup()
     createCanvas(windowWidth, windowHeight);
 }
 
-function drawBars(inner, barLength)
+function drawBars(inner, barLength, rotation = 0)
 {
     push()
-    
+
     translate(windowWidth / 2, windowHeight / 2)
-    let outer = inner + (energy * barLength)
+    rotate(rotation)
+    analyser.getByteFrequencyData(freqData)
 
     for (let i = 0; i < 50; i++) 
     {   
+        let barHeight = freqData[i] * (barLength / 255)
+        let outer = inner + barHeight
+
         let angle = (i/50) * TWO_PI
         //start of bar
         let x1 = cos(angle) * inner
@@ -32,6 +37,7 @@ function drawBars(inner, barLength)
         let x2 = cos(angle) * outer
         let y2 = sin(angle) * outer
 
+        strokeWeight(6)
         line(x1, y1, x2, y2)
     }
 
@@ -42,44 +48,97 @@ function draw()
 {
     switch(mood){
         case "euphoric":
-            background('purple')
-            drawBars(80, 230)
+            background('black')
+            stroke(128, 0, 128)
+            drawBars(100, 230)
             break;
 
         case "calm":
-            background('green')
-            drawBars(80,180)
+            background('black')
+            stroke(46, 139, 87)
+            drawBars(100,180)
             break;
 
         case "aggressive":
-            background('red')
-            drawBars(80, 300)
+            background('black')
+            stroke(220, 20, 60)
+            drawBars(100, 300)
             break;
 
         case "melancholic":
-            background('blue')
-            drawBars(80, 150)
+            background('black')
+            stroke(30, 144, 255)
+            drawBars(100, 150)
             break;
 
         case "tense":
-            background('orange')
-            drawBars(80, 260)
+            background('black')
+            stroke(244, 164, 96)
+            drawBars(100, 260)
             break;
 
         default:
             background('black')
+            stroke(255,255,255)
+            rotation += 0.004
+            drawBars(100,5, rotation)
     }
 }
 
 const audioEl = new Audio()
+const audioContext = new AudioContext()
+const analyser = audioContext.createAnalyser()
+analyser.fftSize = 1024
+
+const source = audioContext.createMediaElementSource(audioEl)
+source.connect(analyser)
+analyser.connect(audioContext.destination)
+
+const freqData = new Uint8Array(analyser.frequencyBinCount)
 
 document.querySelector('#play-btn').addEventListener('click', () => {
-    audioEl.paused ? audioEl.play() : audioEl.pause()
+     if (audioEl.paused) {
+        audioEl.play()
+        document.querySelector('#play-btn').textContent = '⏸'
+    } else {
+        audioEl.pause()
+        document.querySelector('#play-btn').textContent = '▶'
+    }
 })
 
 document.querySelector('#volume').addEventListener('input', (e) => {
     audioEl.volume = e.target.value
 })
+
+audioEl.addEventListener('loadedmetadata', () => {
+    document.querySelector('#duration').textContent = formatTime(audioEl.duration)
+    document.querySelector('#progress').max = audioEl.duration
+})
+
+audioEl.addEventListener('timeupdate', () => {
+    document.querySelector('#current-time').textContent = formatTime(audioEl.currentTime)
+    document.querySelector('#progress').value = audioEl.currentTime
+})
+
+audioEl.addEventListener('ended', () => {
+    audioEl.currentTime = 0
+    document.querySelector('#progress'),value = '0:00'
+    document.querySelector('#current-time').textContent = '0:00'
+})
+
+document.querySelector('#upload-btn').addEventListener('click', () => {
+    document.querySelector('#audio-file').click()
+})
+
+document.querySelector('#progress').addEventListener('input', (e) => {
+    audioEl.currentTime = e.target.value
+})
+
+function formatTime(seconds) {
+    const mins = Math.floor(seconds / 60)
+    const secs = Math.floor(seconds % 60)
+    return `${mins}:${secs.toString().padStart(2, '0')}`
+}
 
 document.querySelector('#audio-file').addEventListener('change', (e) => {
     const file = e.target.files[0]
